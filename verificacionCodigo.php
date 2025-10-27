721f6ccd153463542fc1ce4733956f68fabe3b7a
ManesCruz <manuele.cruzp@konradlorenz.edu.co>
2025-10-27 13:23:47 -0500

    ordenar mejor validación codigo front

--- verificacionCodigo.php
+++ verificacionCodigo.php
@@ -12,15 +12,21 @@ if (!isset($_SESSION['2fa_user_id'])) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Verificar Código</title>
 </head>
 <body>
+     <div class="wrapper"></div>
     <h2>Autenticación en dos pasos</h2>
     <p>Hemos enviado un código a tu correo (revisa spam).</p>
 
     <form action="/LoginKonoha/InicioSesion/validarCodigo.php" method="POST">
-        <input type="text" name="codigo" placeholder="Ingrese el código" required maxlength="6" pattern="\d{6}">
-        <button type="submit">Verificar</button>
+        <input type="text" name="codigo" placeholder="Ingrese el código" required maxlength="6" pattern="\d{6}"> 
+                    <input type="submit" value="Validar Codigo">
+                
     </form>
+</div>
 </body>
 </html>
